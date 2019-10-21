bf29f66a5711a6fd71a963b4a6f718c6fd03a9af
martinxi679 <martinxi679@gmail.com>
2019-10-20 20:50:46 -0400

    Update AdminHomeViewController.swift

--- SmartTrainingLog/Smart Training Log/Admin/AdminHomeViewController.swift
+++ SmartTrainingLog/Smart Training Log/Admin/AdminHomeViewController.swift
@@ -7,4 +7,57 @@ import UIKit
 
 class AdminHomeViewController: UIViewController {
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    private func addSession() {
+        guard
+            let title = sessionTitle.text,
+            let name = username.text,
+            let content = sessionContent.text
+        else {
+            return
+        }
+        
+        if
+            let authStore = try? Container.resolve(AuthenticationStore.self),
+            let storageManager = try? Container.resolve(CloudStorageManager.self),
+            let user = authStore.user {
+                
+            let session = ["uid1": user.uid,
+                            "uid2": user2.uid,
+                            "title": title as String,
+                            "name": name as String,
+                            "content": content as String]
+
+            let key = user.uid
+            ref.child(key).setValue(session)
+            }
+    }
 }
